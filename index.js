994656bad643bb080cd0484dab6ec43fc44ae7f7
Arnaud Crohen <133008493+h4okr@users.noreply.github.com>
2024-01-11 00:02:54 +0100

    no message

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import définir from "./définir";
-import objet from "./objet";
+/*
+import _objet from "./objet";
 import Glossaire from "./glossaire";
 
-export const objet = objet;
+export const objet = _objet;
 export const Glossaire = Glossaire;
+*/
 
 export default définir;
